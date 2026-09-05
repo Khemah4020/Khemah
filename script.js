0c33c9167c3ea4dc9a99b748297688bedb1557de
Khemah4020 <amirhosseinjahanfekr@gmail.com>
2026-09-05 18:05:34 +0330

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ document.querySelectorAll(".nav-links a").forEach(link => {
         navLinks.classList.remove("active");
     });
 });
+
+
+/* LIQUID MOUSE EFFECT */
+
+const exploreButton = document.querySelector(".main-button");
+
+exploreButton.addEventListener("mousemove", (event) => {
+    const rect = exploreButton.getBoundingClientRect();
+
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    exploreButton.style.setProperty("--mouse-x", `${x}px`);
+    exploreButton.style.setProperty("--mouse-y", `${y}px`);
+});
+
+exploreButton.addEventListener("mouseleave", () => {
+    exploreButton.style.setProperty("--mouse-x", "50%");
+    exploreButton.style.setProperty("--mouse-y", "50%");
+});
